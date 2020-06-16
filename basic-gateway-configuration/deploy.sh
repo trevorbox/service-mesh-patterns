61de02f57a618f5ae9fb3d0d62a91b9fb6b95670
tbox <tbox@redhat.com>
2020-06-16 12:31:08 -0700

    move app into own template

--- basic-gateway-configuration/deploy.sh
+++ basic-gateway-configuration/deploy.sh
@@ -7,12 +7,19 @@ CONTROL_PLANE_ROUTE_NAME=api
 
 oc new-project ${DEPLOY_NAMESPACE}
 
-echo "Deploy VirtualService & App in namespace \"${DEPLOY_NAMESPACE}\" and Gateway in namespace \"${CONTROL_PLANE_NAMESPACE}\"..."
-helm template -n ${DEPLOY_NAMESPACE} \
+
+echo -e "\nDeploy VirtualService & App in namespace \"${DEPLOY_NAMESPACE}\" and Gateway in namespace \"${CONTROL_PLANE_NAMESPACE}\"...\n"
+
+helm template . -n ${DEPLOY_NAMESPACE} \
   --set control_plane_namespace=${CONTROL_PLANE_NAMESPACE} \
   --set control_plane_name=${CONTROL_PLANE_NAME} \
   --set route_hostname=$(oc get route ${CONTROL_PLANE_ROUTE_NAME} -n ${CONTROL_PLANE_NAMESPACE} -o jsonpath={'.spec.host'}) \
-  . | oc apply -f -
+  | oc apply -f -
 
-echo "Done."
+echo -e "\nDeploy App...\n"
+
+helm template ../app -n ${DEPLOY_NAMESPACE} | oc apply -f -
+
+echo -e "\nDone.\n"
+
 exit 0

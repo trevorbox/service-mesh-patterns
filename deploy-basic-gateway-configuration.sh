1c8de20f001c44769b3574a9cc8d9f7d748e2653
tbox <tbox@redhat.com>
2020-06-16 13:14:43 -0700

    rename app to bookinfo

--- deploy-basic-gateway-configuration.sh
+++ deploy-basic-gateway-configuration.sh
@@ -8,7 +8,7 @@ CONTROL_PLANE_ROUTE_NAME=${control_plane_route_name}
 
 oc new-project ${DEPLOY_NAMESPACE}
 
-echo -e "\nDeploy VirtualService & App in namespace \"${DEPLOY_NAMESPACE}\" and Gateway in namespace \"${CONTROL_PLANE_NAMESPACE}\"...\n"
+echo -e "\nDeploy VirtualService & bookinfo App in namespace \"${DEPLOY_NAMESPACE}\" and Gateway in namespace \"${CONTROL_PLANE_NAMESPACE}\"...\n"
 
 helm template basic-gateway-configuration -n ${DEPLOY_NAMESPACE} \
   --set control_plane_namespace=${CONTROL_PLANE_NAMESPACE} \
@@ -16,9 +16,9 @@ helm template basic-gateway-configuration -n ${DEPLOY_NAMESPACE} \
   --set route_hostname=$(oc get route ${CONTROL_PLANE_ROUTE_NAME} -n ${CONTROL_PLANE_NAMESPACE} -o jsonpath={'.spec.host'}) \
   | oc apply -f -
 
-echo -e "\nDeploy App...\n"
+echo -e "\nDeploy bookinfo App...\n"
 
-helm template app -n ${DEPLOY_NAMESPACE} | oc apply -f -
+helm template bookinfo -n ${DEPLOY_NAMESPACE} | oc apply -f -
 
 echo -e "\nDone.\n"
 
